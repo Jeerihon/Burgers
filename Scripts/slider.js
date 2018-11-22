e10a0fc2adad5006cfe39dc7f26dda508c51ac55
Jeerihon <korart_95@mail.ru>
2018-11-22 22:37:32 +0800

    upd arch

---
+++ Scripts/slider.js
@@ -0,0 +1,12 @@
+  //slider
+
+  const prevBtn = document.querySelector('.slider__arrow-left'),
+        nextBtn = document.querySelector('.slider__arrow-right');
+
+  prevBtn.addEventListener('click', (e) => {
+    console.log('хрю');
+  })
+
+  nextBtn.addEventListener('click', (e) => {
+    console.log('Аня говнюк');
+  })
