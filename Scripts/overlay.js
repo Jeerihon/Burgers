f9bbf25aa40ca46fbdb66fef34e511eb6e3141a8
Jeerihon <korart_95@mail.ru>
2018-11-25 02:53:51 +0800

    overlay upd

--- Scripts/overlay.js
+++ Scripts/overlay.js
@@ -1,30 +1,33 @@
 // // overlay
 
 const openButtons = document.querySelectorAll(".review-block__btn"),
-  reviewsSection = document.querySelector('#reviews'),
-  nameBlocks = document.querySelectorAll('.review-block__name'),
-  textBlocks = document.querySelectorAll('.review-block__text');
+  reviewsSection = document.querySelector('#reviews');
 
 
 for (let i = 0; i < openButtons.length; i++) {
   const element = openButtons[i];
 
   element.addEventListener("click", function () {
-    const name = element.parentNode.children[0].textContent,
-          text = element.parentNode.children[1].textContent;
+    // const name = element.parentNode.children[0].textContent,
+    //       text = element.parentNode.children[1].textContent;
+    const elements = element.parentNode.children;
+    let name, 
+        text;
+
+    for (let i = 0; i < elements.length; i++) {
+      const element = elements[i];
+      if (element.classList.contains('review-block__name')) {
+        name = element.textContent
+      };
+      if (element.classList.contains('review-block__text')) {
+        text = element.textContent
+      };
+
+    }
 
     reviewsSection.appendChild(createOverlay(name, text));
   });
 };
-
-function getTextContent(element) {
-  for (let i = 0; i < element.length; i++) {
-    const item = element[i];
-
-    const content = item.textContent;
-    return content;
-  };
-}
 
 function createOverlay(name, text) {
   const overlayElement = document.createElement("div");
