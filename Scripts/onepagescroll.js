5e42d5000ed629d01e290521c152ae044ed41921
Jeerihon <korart_95@mail.ru>
2018-11-30 15:59:12 +0800

    shrink-to-fit

--- Scripts/onepagescroll.js
+++ Scripts/onepagescroll.js
@@ -137,7 +137,7 @@ $(function () {
 
       if (reqItem.length) {
         list.animate({
-          'top': -reqIndex * 100 + '%'
+          'top': -reqIndex * 100 + 'vh'
         }, duration, function () {
           activeSection.removeClass('active');
           reqItem.addClass('active');
