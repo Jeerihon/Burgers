42c89e9b69ed4874890b2d2db6af9fd5f0d1db7c
Jeerihon <korart_95@mail.ru>
2018-11-30 14:22:31 +0800

    ops upd for mobile

--- Scripts/onepagescroll.js
+++ Scripts/onepagescroll.js
@@ -84,36 +84,6 @@ $(function () {
 
   //touch slide
 
-  // $('.wrapper').swipe({
-  //   swipeStatus: function (event, phase, direction, distance, duration, fingerCount, fingerData, currentDirection) {
-  //     let $this = $(this),
-  //       container = $this.closest('.wrapper'),
-  //       items = $('.section', container),
-  //       activeItem = items.filter('.active'),
-  //       nextItem = activeItem.next(),
-  //       prevItem = activeItem.prev(),
-  //       existedItem, edgeItem, reqItem;
-
-  //     if (phase == "end") {
-  //       //сработает через 20 пикселей то число которое выбрали в threshold
-  //       if (direction == 'up') {
-  //         existedItem = activeItem.prev(),
-  //           edgeItem = items.last(),
-  //           reqItem = existedItem.length ? existedItem.index() : edgeItem.index(); //сработает при движении вверх
-  //       }
-  //       if (direction == 'down') {
-  //         existedItem = activeItem.next(),
-  //           edgeItem = items.first(),
-  //           reqItem = existedItem.length ? existedItem.index() : edgeItem.index();  //сработает при движении вниз
-  //       }
-
-
-  //       moveSection(container, reqItem);
-  //     }
-  //     triggerOnTouchEnd:false,
-  //     threshold:20
-  //   }
-  // });
   $(".wrapper").swipe({
     //Single swipe handler for left swipes
     swipeUp: function (event, direction, distance, duration, fingerCount) {
@@ -140,7 +110,7 @@ $(function () {
 
         moveSection(container, reqItem);
     },
-    threshold: 75
+    threshold: 0
   });
 
   // quick clicks protection
