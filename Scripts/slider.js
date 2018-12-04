b6870e336fb1f818a820464c8183952535f88a60
Jeerihon <korart_95@mail.ru>
2018-12-05 00:09:04 +0800

    slider upd

--- Scripts/slider.js
+++ Scripts/slider.js
@@ -11,8 +11,6 @@ const left = document.querySelector('.slider__arrow-left'),
   maxRight = size * step,
   minRight = 0;
 
-console.log(maxRight)
-
 var flag = true;
 
 var changeFlag = function () {
@@ -30,7 +28,7 @@ var timerID = setInterval(function () {
 right.addEventListener("click", function (e) {
   e.preventDefault();
 
-  moveRight();
+  move(right);
   clearInterval(timerID);
 });
 
@@ -38,55 +36,43 @@ right.addEventListener("click", function (e) {
 left.addEventListener("click", function (e) {
   e.preventDefault();
 
-  moveLeft();
+  move(left);
   clearInterval(timerID);
 });
 
-function moveRight() {
+function move(direction) {
   let items = document.querySelector('.slider'),
     computed = getComputedStyle(items),
     itemWidth = parseInt(getComputedStyle(items.firstElementChild).width),
-    currentRightValue = parseInt(computed.right) / itemWidth * 100,
-    currentRight = Math.round(currentRightValue / 100) * 100;
-  console.log(currentRight)
-  if (flag) {
+    currentRight = parseInt(computed.right) / itemWidth * 100;
+
+  if (flag) { //multuclick protection
     flag = false
+
     if (!currentRight) {
       currentRight = 0;
     }
 
-    if (currentRight < maxRight) {
-      items.style.right = currentRight + step + '%';
-    } else {
-      currentRight = 0;
-      items.style.right = minRight;
-    };
+    // slide to right
+    if (direction == right) {
+      if (currentRight < maxRight) {
+        items.style.right = currentRight + step + '%';
+      } else {
+        currentRight = 0;
+        items.style.right = minRight;
+      };
+    }
+    // slide to left
+    if (direction == left) {
+      if (currentRight > minRight) {
+        items.style.right = currentRight - step + "%";
+      };
+      if (currentRight == 0) {
+        currentRight = maxRight;
+        items.style.right = maxRight + "%";
+      };
+    }
 
     changeFlag();
   };
-}
-
-function moveLeft() {
-  let items = document.querySelector('.slider'),
-    computed = getComputedStyle(items),
-    itemWidth = parseInt(getComputedStyle(items.firstElementChild).width),
-    currentRightValue = parseInt(computed.right) / itemWidth * 100,
-    currentRight = currentRightValue
-  if (flag) {
-    flag = false
-
-    if (!currentRight) {
-      currentRight = 0;
-    }
-
-    if (currentRight > minRight) {
-      items.style.right = currentRight - step + "%";
-    };
-    if (currentRight == 0) {
-      currentRight = maxRight;
-      items.style.right = maxRight + "%";
-    };
-
-    changeFlag();
-  }
 }
